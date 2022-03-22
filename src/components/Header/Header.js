b3a63a59f58d203fe2e3968d1e37fabd135645fd
prokash Pul <pprokash102@gmail.com>
2022-03-22 10:11:47 +0600

    hrader menu update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
-  return <div>Nav bar</div>;
+  return (
+    <header className="header">
+      <nav className="header-nav">
+        <div className="logo">
+          <img src={logo} alt="EmaJonShop" height="40" width="140" />
+        </div>
+        <ul className="nav-items">
+          <li className="nav-item">
+            <a href="/shop">Shop</a>
+          </li>
+          <li className="nav-item">
+            <a href="/order">Order</a>
+          </li>
+          <li className="nav-item">
+            <a href="/order-review">Order Review</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
