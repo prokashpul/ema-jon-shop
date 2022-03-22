18861cf8615881306d3e77dcf14b3586683d74c8
prokash Pul <pprokash102@gmail.com>
2022-03-22 11:02:54 +0600

    mobile menu update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../images/Logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  // mobile nav bar open and close
+  const openBar = () => {
+    document.querySelector(".open-nav").style.display = "none";
+    document.querySelector(".nav-items").style.left = "0";
+    document.querySelector(".close-nav").style.display = "block";
+  };
+  const closeBar = () => {
+    document.querySelector(".close-nav").style.display = "none";
+    document.querySelector(".open-nav").style.display = "block";
+    document.querySelector(".nav-items").style.left = "-100%";
+  };
   return (
     <header className="header">
       <nav className="header-nav">
@@ -19,10 +32,16 @@ const Header = () => {
           <li className="nav-item">
             <a href="/order-review">Order Review</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" onClick={openBar}>
             <a href="/about">About</a>
           </li>
         </ul>
+        <div className="open-nav" onClick={openBar}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <div className="close-nav" onClick={closeBar}>
+          <FontAwesomeIcon icon={faXmark} />
+        </div>
       </nav>
     </header>
   );
